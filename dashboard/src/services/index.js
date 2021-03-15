e0002f362799f5f1a539f82b6f9350e0262caf76
Jonathan S. Machado <jonathan.machado@MacBook-Pro-de-Jonathan.local>
2021-03-14 22:10:26 -0300

    updating files

--- dashboard/src/services/index.js
+++ dashboard/src/services/index.js
@@ -6,12 +6,12 @@ import UsersService from './users'
 import FeedbacksService from './feedbacks'
 
 const API_ENVS = {
-  local: 'http://localhost:3000',
-  production: 'https://backend-treinamento-vue3-nine.vercel.app'
+  production: 'https://backend-treinamento-vue3-nine.vercel.app',
+  local: 'http://localhost:3000'
 }
 
 const httpClient = axios.create({
-  baseURL: API_ENVS[process.env.NODE_ENV] ?? API_ENVS.local
+  baseURL: API_ENVS[process.env.NODE_ENV] || API_ENVS.local
 })
 
 httpClient.interceptors.request.use(config => {
--- dashboard/tests/e2e/specs/credentials.js
+++ dashboard/tests/e2e/specs/credentials.js
@@ -8,10 +8,14 @@ describe('Credentials', () => {
     cy.get('#email-field').type('igor@igor.me')
     cy.get('#password-field').type('1234')
     cy.get('#submit-button').click()
+
+    cy.wait(3000)
     cy.get('#menu-credentials-button').click()
+    cy.wait(2000)
 
     const oldApikey = cy.get('#apikey-container').invoke('text')
     cy.get('#generate-apikey-button').click()
+    cy.wait(2000)
     const newApikey = cy.get('#apikey-container').invoke('text')
 
     expect(oldApikey).to.not.equal(newApikey)
